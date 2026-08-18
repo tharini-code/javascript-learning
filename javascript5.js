b2dd57b4ce2b187c5fcb71ca02a6584d2ff07727
tharini-code <gdharanidharan3@gmail.com>
2026-08-19 00:37:15 +0530

    learining switch n case

---
+++ javascript5.js
@@ -0,0 +1,23 @@
+var marks=63
+var gread
+switch(true){
+    case marks>=90:
+        gread="A"
+        break
+    case marks>=80:
+        gread="B"
+        break
+    case marks>=70:
+        gread="C"
+        break 
+    case marks>=60:
+        gread="D"
+        break
+    case marks>=50:
+        gread="E"
+        break
+    default:
+        gread="F"        
+
+}
+console.log(gread)
